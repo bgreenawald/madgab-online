bacecf9075608a4c028c3df201b302324d8e9e6e
Benjamin Greenawald <bgreenawald@gmail.com>
2019-07-26 21:41:21 -0400

    Game fields added, documentation added

--- app.py
+++ app.py
@@ -2,8 +2,9 @@ import random
 
 from flask import Flask, render_template
 from flask_socketio import join_room, leave_room, SocketIO, send, emit
+import simplejson
 
-from game import Game
+from game import Game, State
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'secret!'
@@ -11,6 +12,7 @@ socketio = SocketIO(app)
 
 all_games = {}
 
+# App routes
 @app.route('/')
 def return_index():
     return render_template("index.html")
@@ -19,30 +21,68 @@ def return_index():
 def return_game(id=None):
     return render_template("game.html", id=id)
 
-
+# Socket functions
 @socketio.on('join')
 def on_join(data):
+    """Joins the connection to the provided room
+
+    Args:
+        data (str): Name of the room.
+    """
     room = data
     join_room(room)
     print(f'User has entered the room {room}')
+    print(f'{socketio.server.rooms}')
 
 @socketio.on('leave')
 def on_leave(data):
+    """Removes the current connection from the room.
+
+    Args:
+        data (str): Name of the room.
+    """
     room = data['room']
     leave_room(room)
     print(f'User has left the room {room}', room=room)
 
 @socketio.on('load_board')
-def load_game_board(json):
-    game = json["game"]
+def load_board(json):
+    """Loads the current game board, or creates on if none exists.
+
+    Args:
+        json (dict): dictionary with parameter called 'game'.
+    """
+    try:
+        game = json["name"]
+    except KeyError:
+        emit("render_board", {
+            "status_code": 400,
+            "message": "'name' not supplied",
+            "payload": {}
+
+        }, room=game)
+        return
+
     print(f"Loading board {game}")
     if game in all_games:
         cur_game = all_games[game]
-        emit("render_board", {"game": str(cur_game)}, room=game)
+        emit("render_board", {
+            "status_code": 200,
+            "message": "Game retreived",
+            "payload": simplejson.dumps(cur_game, for_json=True)
+
+        }, room=game)
     else:
-        cur_game = Game(game, 15)
+        cur_game = Game(game)
         all_games[game] = cur_game
-        emit("render_board", {"game": str(cur_game)}, room=game)
+        emit("render_board", {
+            "status_code": 200,
+            "message": "New game created",
+            "payload": {
+                simplejson.dumps(cur_game, for_json=True)
+            }
+        }, room=game)
+
 
 if __name__ == '__main__':
     socketio.run(app, debug=True)
--- game.py
+++ game.py
@@ -1,4 +1,14 @@
 import datetime
+from enum import Enum
+import random
+
+all_games = {}
+
+class State(Enum):
+    IDLE = "IDLE"
+    ACTIVE = "ACTIVE"
+    STEALING = "STEALING"
+    OVER = "OVER"
 
 # Define the boundaries on games
 MAX_THRESHOLD = 50
@@ -6,18 +16,58 @@ MIN_THRESHOLD = 1
 
 
 class Game(object):
-    def __init__(self, id, win_threshold):
+    """
+    A class representing an entire game.
+
+    Attributes:
+        id (str): Id of a game.
+        win_threshold (int): Minimum score needed to win a game. Defaults 30.
+        words_per_turn (int): Number of words per turn. Defaults 3.
+        seconds_per_turn (int): Number of seconds in a turn. Defaults 60.
+        date_created (datetime): When the game was first created.
+        team_1_score (int): Team 1's score.
+        team_2_score (int): Team 2's score.
+        state (State): The current state of the game. Can be "IDLE", "ACTIVE", "STEALING", "OVER"
+        clues (list): All of the clues for a given game.
+        team_1_turn (bool): Whether it is team 1's turn.
+        winning_team (str): The winning team. Can be None, 'Team 1', or 'Team 2'
+        current_phrase (str): The current phrase (plain)
+        current_madgab (str): The madgabed version of the current phrase
+        current_turn_clues (list of tuples): The clues for the current turn.
+            Each list element is a tuple of length 3 with the original phrase, the madgabed phrase,
+            and a boolean for whether the team got the phrase correctly.
+        current_turn_counter (int): The number of words guessed so far this turn.
+        current_turn_correct (int): The number of words guessed correctly so far this turn.
+    """
+
+    def __init__(self, id, win_threshold=30, words_per_turn=3, seconds_per_turn=60):
         self.id = id
-        # Make sure threshold falls within boundaries
+
+        # General game configuration
         self.win_threshold = min(max(MIN_THRESHOLD, win_threshold), MAX_THRESHOLD)
+        self.words_per_turn = words_per_turn
+        self.seconds_per_turn = 60
+        self.date_created = datetime.datetime.now()
+
+        # Game state
         self.team_1_score = 0
         self.team_2_score = 0
+        self.state = State.IDLE
+
         # Initialize the clues
         with open("./clues.txt", "r") as file:
-            self.clues = [clue.strip() for clue in file.readlines()]
+            clues = [clue.strip() for clue in file.readlines()]
+            random.shuffle(clues)
+            self.clues = clues
+
+        # Turn state
         self.team_1_turn = True
-        self.date_created = datetime.datetime.now()
         self.winning_team = None
+        self.current_word = ""
+        self.current_madgab = ""
+        self.current_turn_clues = []
+        self.current_turn_counter = 0
+        self.current_turn_correct = 0
 
     def __str__(self):
         return (
@@ -31,5 +81,26 @@ class Game(object):
             f"Winning team: {self.winning_team}"
         )
 
+    def __json__(self):
+        return {
+            "id": self.id,
 
+            # General game configuration
+            "win_threshold": self.win_threshold,
+            "words_per_turn": self.words_per_turn,
+            "seconds_per_turn": self.seconds_per_turn,
 
+            # Game state
+            "team_1_score": self.team_1_score,
+            "team_2_score": self.team_2_score,
+            "state": self.state,
+            "winning_team": self.winning_team,
+
+            # Turn state
+            "team_1_turn": self.team_1_turn,
+            "current_turn_clues": self.current_turn_clues,
+            "current_turn_counter": self.current_turn_counter,
+            "current_turn_correct": self.current_turn_correct,
+        }
+
+        for_json = __json__
