cd15b2238aec4a2b6c0b6dadb38c95e322dad7bd
unknown <seaweed4@live.com>
2019-08-18 20:47:09 -0400

    functioning layout for before turn starts

--- frontend/src/components/Game.js
+++ frontend/src/components/Game.js
@@ -1,6 +1,7 @@
 import React, {Component} from 'react';
 import '../App.scss'
 import Header from './Header'
+import socketIOClient from 'socket.io-client'
 
 class Game extends Component {
     constructor(props) {
@@ -8,13 +9,19 @@ class Game extends Component {
         this.state = {
             //  href_parts = window.location.href.split("/"),
             active_team: 1,
+            game_id: this.props.history.location.state.game_id,
              game_name: this.props.history.location,
              game_state: null,
+             response: null,
+             userRole: null,
              scores: [0,0],
              team: null,
              turn_timer: null,
              timer: null
         }
+        this.clueRadioYes = React.createRef();
+        this.clueRadioNo = React.createRef();
+        this.startTurn = React.createRef();
     }
 
     handleTeamSelection = e => {
@@ -26,20 +33,61 @@ class Game extends Component {
         })
     }
 
-    componentDidMount = () => {
-        socket.on("connect", function() {
-            socket.emit("join", game_name);
-        });
+    //  componentDidMount =  () => {
+    // when exactly to use component did mount?????????? !<KMLFJEALJNFL
     
-        // Load the current state of the board, or create a new one
-        socket.on("connect", function() {
-            socket.emit("load_board", {
-                "name": game_name
-            })
+    // }
+
+    joinGame = () => {
+        var socket = socketIOClient('http://localhost:5000')
+        socket.on("connect", () => {
+            socket.emit("join", this.state.game_id);
         });
     }
 
+     //     // Load the current state of the board, or create a new one
+     loadBoard = () => {
+         console.log("GAME ID", this.state.game_id)
+         var socket = socketIOClient('http://localhost:5000')
+         socket.on("connect", () => {
+             socket.emit("load_board", {
+                 "name": this.state.game_id
+             })
+         })
+     }
+         
+
+    renderBoard = () => {
+        var socket = socketIOClient('http://localhost:5000')
+        socket.on("render_board", resp => {
+            console.log("RESP", resp)
+            let game_state = JSON.parse(resp.payload)
+            console.log(game_state)
+            this.setState({
+                response: game_state
+            })
+        })
+    }
+
+    handleGameStart = () => {
+        this.setState({
+            turn_timer: 90
+        })
+    }
+
+    handleRoleSelected = () => {
+        // console.log("is yes checked?", this.clueRadioYes.current.value) 
+        this.setState({
+            userRole: document.querySelector('input[name="userRole"]:checked').value
+        })
+    }
+
     render() {
+        console.log("STATE", this.state)
+        this.joinGame()
+        this.loadBoard()
+        this.renderBoard()
+
         if (this.state.team) {
             console.log("You chose a team!")
             return(
@@ -50,14 +98,29 @@ class Game extends Component {
         else {
             // choose your team
             return(
-                <div className="game-viewport">
+            <div className="game-viewport">
 
-            <div className="choose-team container">
-                <Header props={this.state.scores}/>
-                <h1>Choose your team:</h1>
-                <button onClick={this.handleTeamSelection} data="1">Team 1</button>
-                <button onClick={this.handleTeamSelection} data="2">Team 2</button>
-            </div>
+                <div className="turn-container">
+                    <h3>It's your turn next:</h3>
+                    <h2>Team 1</h2>
+                </div>
+
+                {/* <div className="choose-team container">
+                    <Header props={this.state.scores}/>
+                    <h1>Choose your team:</h1>
+                    <button onClick={this.handleTeamSelection} data="1">Team 1</button>
+                    <button onClick={this.handleTeamSelection} data="2">Team 2</button>
+                </div> */}
+
+                <div className="game-options">
+                    <p>Are you the clue giver?</p>
+                    <input type="radio" name="userRole" ref={this.clueRadioYes} onChange={this.handleRoleSelected} value="clue giver"/>
+                    <label htmlFor="Yes">Yes</label>
+                    <input type="radio" name="userRole"  ref={this.clueRadioNo} onChange={this.handleRoleSelected} value="clue guesser"/>
+                    <label htmlFor="No">No</label>
+                </div>
+
+                <button id="start-turn" ref={this.startTurn} disabled={this.state.userRole === null} onClick={this.renderBoard}>Start the turn!</button>
             </div>
 
             )
