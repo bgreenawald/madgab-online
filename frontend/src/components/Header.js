ef3b131629e85b3304d62dfbd8a5995f60b6af25
Jocelyn Huang <jocelynyh4@gmail.com>
2019-10-31 01:21:34 -0400

    Fixed background styling for landing and game pages

---
+++ frontend/src/components/Header.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+import '../App.scss'
+
+class Header extends Component {
+    constructor(props) {
+        super(props)
+    }
+
+    render() {
+        { this.state && console.log(this.state) }
+        return (
+            <div className="header flex">
+                    <h3>Round 1</h3>
+                    <div className="score-box">
+                        <span className="current-team-score">0 -</span>
+                        <span className="opposing-team-score"> 0</span>
+                    </div>
+            </div>
+        )
+    }
+}
+
+export default Header
