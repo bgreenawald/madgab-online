1f3eeb2ed830a4731d95467c576d6e972ec1b943
Benjamin Greenawald <bgreenawald@gmail.com>
2019-10-31 20:37:20 -0400

    Fixed frequency matrix bug

--- madgab/madgab.py
+++ madgab/madgab.py
@@ -22,6 +22,39 @@ FREQUENCY_FILENAME = os.path.join(
     "frequency.csv"
 )
 
+class FrequencyMatrix(object):
+    """
+    Wrapper function around the frequency matrix.
+
+    Main tasks include handling indexing and normalization.
+    """
+
+    def __init__(self, filename=FREQUENCY_FILENAME):
+        self.frequency_matrix = pd.read_csv(filename, header=None)
+        self.normalize()
+
+    def __getitem__(self, tup):
+        x, y = tup
+
+        def make_index(ind: str) -> int:
+            if ind == " ":
+                return 26
+            else:
+                return ord(ind) - ord('a')
+
+        x_ind = make_index(x)
+        y_ind = make_index(y)
+
+        if not 0 <= x_ind <= 26 or not 0 <= y_ind <= 26:
+            raise(KeyError("Indexes should be lowercase letters or spaces."))
+
+        return self.frequency_matrix.iloc[x_ind, y_ind]
+
+    def normalize(self):
+        self.frequency_matrix = self.frequency_matrix.div(
+            self.frequency_matrix.sum(axis=1), axis=0
+        )
+
 def word_to_pronunciation(word: str, word_to_phoneme: dict, phoneme_to_pronunciation:dict) -> str:
     """ Task a word a returns its phonetic spelling. """
     # Get the phoneme list for the word (choose random pronunciation).
@@ -130,39 +163,6 @@ def add_spaces(phrase: str, freq: FrequencyMatrix) -> str:
             cur_len = 1
 
     return end_str
-
-class FrequencyMatrix(object):
-    """
-    Wrapper function around the frequency matrix.
-
-    Main tasks include handling indexing and normalization.
-    """
-
-    def __init__(self, filename=FREQUENCY_FILENAME):
-        self.frequency_matrix = pd.read_csv(filename, header=None)
-        self.normalize()
-
-    def __getitem__(self, tup):
-        x, y = tup
-
-        def make_index(ind: str) -> int:
-            if ind == " ":
-                return 26
-            else:
-                return ord(ind) - ord('a')
-
-        x_ind = make_index(x)
-        y_ind = make_index(y)
-
-        if not 0 <= x_ind <= 26 or not 0 <= y_ind <= 26:
-            raise(KeyError("Indexes should be lowercase letters or spaces."))
-
-        return self.frequency_matrix.iloc[x_ind, y_ind]
-
-    def normalize(self):
-        self.frequency_matrix = self.frequency_matrix.div(
-            self.frequency_matrix.sum(axis=1), axis=0
-        )
 
 
 def tester():
