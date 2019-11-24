244e58569b164370add054867962fa2089694e7a
Jocelyn Huang <jocelyn@Jocelyns-MacBook-Pro.local>
2019-11-23 21:51:38 -0500

    begin integrating redux

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,10 +1,21 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import './Styles/index.css';
+import {ReactDOM, render} from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import rootReducer from './reducers';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import './Styles/index.css';
+
+const store = createStore(rootReducer);
+
+render(
+    <Provider store={store}>
+        <App/>
+    </Provider>,
+    document.getElementById('root')
+)
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
