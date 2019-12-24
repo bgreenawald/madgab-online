e2d38fd12148c0563faa9f2a24b8dc4ce367fa9c
Jocelyn Huang <seaweed4@live.com>
2019-12-23 22:09:11 -0500

    Update header

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,18 +1,28 @@
 import React, { Component } from "react";
 import "../Styles/App.scss";
+import "../Styles/Header.scss";
+
+import { connect } from 'react-redux';
 
 class Header extends Component {
   render() {
     return (
       <div className="header">
-        <h3>Round 1</h3>
+        <h3 className="round-counter">Round 1</h3>
         <div className="score-box">
-          <span className="current-team-score">0 -</span>
-          <span className="opposing-team-score"> 0</span>
+          <span className="team-1-blue-score">{this.props.state.team_1_score}</span>
+          <span> - </span>
+          <span className="team-2-red-score">{this.props.state.team_2_score}</span>
         </div>
       </div>
     );
   }
 }
 
-export default Header;
+const mapStateToProps = (state) => {
+  return {
+    state: { ...state }
+  }
+}
+
+export default connect(mapStateToProps)(Header);
