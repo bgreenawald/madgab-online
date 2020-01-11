c445a02ab2641703cb1f74dcafe622165260f3dc
Benjamin Greenawald <bgreenawald@gmail.com>
2020-01-11 10:41:34 -0500

    Refactor repository, closes #8

--- tests/check.ps1
+++ tests/check.ps1
@@ -1,9 +1,9 @@
 # Run flake8
 Write-Output "Running flake8...";
 flake8 .;
-Write-Output "Completed flake8`r`n";
+Write-Output "Completed flake8.`r`n";
 
 # Run mypy
 Write-Output "Running mypy...";
 mypy madgab/ game.py app.py;
-Write-Output "Completed mypy`r`n";
+Write-Output "Completed mypy.";
