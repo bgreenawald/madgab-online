c602514d3e5457501cf020fb5e284180c231a87d
Jocelyn Huang <seaweed4@live.com>
2020-01-21 20:50:53 -0500

    Save progress on animations

--- frontend/src/components/Stealing.js
+++ frontend/src/components/Stealing.js
@@ -34,14 +34,13 @@ class Stealing extends Component {
         this.myElement = null;
         this.myTween = new TimelineLite({ paused: true });
         this.cluesIcons = [];
+        this.references = [];
     }
     componentDidMount = () => {
         this.props.updateGameData({
             inCountdown: true
         });
-        this.myTween = new TimelineLite({ paused: false })
-            .to(this.myElement, 0.5, { y: -100, opacity: 1 })
-            .play();
+        this.myTween.staggerTo(this.cluesIcons, 0.5, { autoAlpha: 1, y: 0 }, 0.1).play();
     }
 
     generateScoreArray = () => {
@@ -50,10 +49,18 @@ class Stealing extends Component {
 
     displayAnswerResults = () => {
         let scoreArray = this.props.state.current_turn_clues;
-        for (let result of scoreArray) {
-            let answer = result[2] ? 'correct' : 'incorrect';
-            this.cluesIcons.push(<ClueIcon value={answer} />)
-        }
+        let index = 0;
+        // for (let result of scoreArray) {
+        //     let answer = result[2] ? 'correct' : 'incorrect';
+        //     this.cluesIcons.push(<ClueIcon value={answer} ref={elem => this.cluesIcons[index] = elem} />)
+        //     index++;
+        // }
+        scoreArray.map((e, i) => (
+            <ClueIcon
+                key={e.id}
+                value={e[2] ? 'correct' : 'incorrect'}
+                ref={elem => this.cluesIcons[i] = e} />
+        ))
     }
 
     submitSteal = () => {
@@ -81,7 +88,7 @@ class Stealing extends Component {
     }
 
     render() {
-        this.displayAnswerResults();
+        // this.displayAnswerResults();
         let currentTeam = this.props.state.team_1_turn ? "Blue Team" : "Red Team";
         let opposingTeam = this.props.state.team_1_turn ? "Red Team" : "Blue Team";
 
@@ -89,7 +96,12 @@ class Stealing extends Component {
             <div className="game-content">
                 <h2>{currentTeam},  you scored: </h2>
                 <div className="clue-icon-container">
-                    {this.cluesIcons}
+                    {/* {this.cluesIcons} */}
+                    {this.props.state.current_turn_clues.map((e, i) => (
+                        <ClueIcon
+                            value={e[2] ? 'correct' : 'incorrect'}
+                            ref={ClueIcon => this.cluesIcons[i] = ClueIcon} />
+                    ))}
                 </div>
                 <div className="square" ref={elem => this.myElement = elem}></div>
                 <h1>{this.getPoints(this.props.state.current_turn_correct)}</h1>
