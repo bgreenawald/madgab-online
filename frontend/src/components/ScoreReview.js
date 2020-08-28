203fd57f46152178c633aa150425aead399ba3d4
Jocelyn Huang <jocelyn@Jocelyns-MacBook-Pro.local>
2020-08-28 02:26:43 -0400

    review animation v1

--- frontend/src/components/ScoreReview.js
+++ frontend/src/components/ScoreReview.js
@@ -24,14 +24,13 @@ class ScoreReview extends Component {
     componentDidMount() {
         this.animation
             .from(
-                '.circle-icon', {
+                '.circle-icon:not(.total)', {
                 duration: 1,
                 opacity: 0,
                 y: -30,
                 ease: 'power1',
                 stagger: 0.2
-            }
-            )
+            })
             .to(
                 '.circle-icon.incorrect', {
                 duration: 1,
@@ -39,21 +38,64 @@ class ScoreReview extends Component {
                 y: 25,
                 ease: 'power3(1,0.3)',
             }, '-=0.2')
-            .to(
-                '.circle-icon.correct', {
-                duration: 2,
-                y: 50,
-                ease: 'power3(1,0.3)',
-                stagger: 0.2
-            }, '-=1.2')
+            // .to(
+            //     '.circle-icon.correct', {
+            //     duration: 2,
+            //     y: 50,
+            //     ease: 'power3(1,0.3)'
+            // }, '-=1.2')
+            .to('.circle-icon:nth-child(1)', {
+                duration: 1,
+                x: 70,
+                y: 25,
+                ease: 'power1.in'
+            }, '-=0.2')
+            .to('.circle-icon:nth-child(3)', {
+                duration: 1,
+                x: -70,
+                y: 25,
+                ease: 'power1.in'
+            }, '-=1')
+            // .from('.circle-icon.total', {
+            //     opacity: 0,
+            //     scale: 0,
+            //     ease: 'back'
+            // }, '-=0.3')
+            .to('.circle-icon:not(.total)', {
+                duration: .7,
+                opacity: 0,
+                ease: "back"
+            }, '-=1')
             .from(
                 'h1#points-count', {
-                    duration: 1,
-                    y: 30,
-                    ease: 'power1',
-                    opacity: 0
-                }, '-=0.5'
-            )
+                duration: 1,
+                y: 30,
+                ease: 'power3',
+                opacity: 0,
+                // onComplete: this.test
+            }, '-=.7')
+            .from(
+                'button.primary', {
+                duration: 1,
+                y: 30,
+                ease: 'power3',
+                opacity: 0,
+                // onComplete: this.test
+            }, '-=.3')
+    }
+
+    test() {
+        // console.log('finished');
+        // const correctIcons = Array.from(document.querySelectorAll('.circle-icon.correct'));
+        // correctIcons.forEach(icon => {
+        //     icon.classList.add('converge');
+        // })
+        const total = document.querySelector('.circle-icon.total');
+        total.classList.remove('hidden');
+        total.classList.add('show');
+
+        const clueIconContainer = document.querySelector('.clue-icon-container');
+        clueIconContainer.classList.add('none');
     }
 
     getPoints = (pts) => {
@@ -85,6 +127,7 @@ class ScoreReview extends Component {
                             key={i}
                             isButton={false} />
                     ))}
+                    <div className="circle-icon total hidden">4</div>
                 </div>
                 {/* <div className="square" ref={elem => this.myElement = elem}></div> */}
                 <h1 id='points-count'>{totalPoints}</h1>
