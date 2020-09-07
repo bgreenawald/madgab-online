09d8c417be083c25a2e7306167199012ee14155a
Jocelyn Huang <jocelyn@Jocelyns-MacBook-Pro.local>
2020-09-07 16:19:18 -0400

    Fix header initial rendering

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -5,24 +5,32 @@ import { connect } from 'react-redux';
 
 class Header extends Component {
   render() {
-    return (
-      <div className="header">
-        <h3 className="round-counter">Round {this.props.state.round_number}</h3>
-        <div className="score-box">
-          <span className="team-1-blue-score">{this.props.state.team_1_score}</span>
-          <span> - </span>
-          <span className="team-2-red-score">{this.props.state.team_2_score}</span>
-        </div>
-        <div className="game-branding">
-          <div className="footer-child">
-            <p>Rad Gab</p>
+    // if (this.props.state.team_1_score) {
+      return (
+        <div className="header">
+          <h3 className="round-counter">Round {this.props.state.round_number}</h3>
+          <div className="score-box">
+            <span className="team-1-blue-score">{this.props.state.team_1_score ? this.props.state.team_1_score : 0}</span>
+            <span> - </span>
+            <span className="team-2-red-score">{this.props.state.team_2_score ? this.props.state.team_2_score : 0}</span>
           </div>
-          <div className="footer-child">
-            <p className="rules-button">Rules</p>
+          <div className="game-branding">
+            <div className="footer-child">
+              <p>Rad Gab</p>
+            </div>
+            <div className="footer-child">
+              <p className="rules-button">Rules</p>
+            </div>
           </div>
         </div>
-      </div>
-    );
+      );
+    // } else {
+    //   return (
+    //     <div className="loading-container">
+    //       <div className="loader"></div>
+    //     </div>
+    //   )
+    // }
   }
 }
 
