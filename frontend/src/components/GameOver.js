5d205c9cf5e224705dacfc05ccab33820990aab9
Jocelyn Huang <jocelyn@Jocelyns-MacBook-Pro.local>
2020-09-27 01:16:20 -0400

    Get confetti cannon working

--- frontend/src/components/GameOver.js
+++ frontend/src/components/GameOver.js
@@ -1,156 +1,159 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
 import { connect } from "react-redux";
-import { updateGameData } from '../store/actions';
+import { updateGameData } from "../store/actions";
 
 import { gsap } from "gsap";
 import { Physics2DPlugin } from "gsap/Physics2DPlugin";
 import { TimelineLite } from "gsap/all";
 
 import "../Styles/Confetti.scss";
+import ConfettiParticle from "./Confetti";
 
 gsap.registerPlugin(Physics2DPlugin);
 
-
 class GameOver extends Component {
-    constructor(props) {
-        super(props)
-        // this.myTween = null;
-        this.gameover = null;
-        this.button = null;
-        this.state = {
-            confetti: []
-        }
-        this.confettiArray = [];
-        this.myTween = new TimelineLite({ paused: true });
-    }
-
-    componentDidMount() {
-        gsap.registerPlugin(Physics2DPlugin);
-        this.myTween.staggerTo(this.confettiArray, 0.5, { y: 0, autoAlpha: 1 }, 0.1);
-    }
-
-    popConfetti(elem) {
-        elem.classList.add('success');
-        let ref = this;
-        gsap.to(elem, {
-            '--icon-x': -3,
-            '--icon-y': 3,
-            '--z-before': 0,
-            duration: .2,
-            onComplete() {
-                ref.particles(elem, 100, -14, 16, -180, 180);
-                gsap.to(elem, {
-                    '--icon-x': 0,
-                    '--icon-y': 0,
-                    '--z-before': -6,
-                    duration: 1,
-                    ease: 'elastic.out(1, .5)',
-                    onComplete() {
-                        elem.classList.remove('success');
-                    }
-                });
-            }
-        });
+  constructor(props) {
+    super(props);
+    this.gameover = null;
+    this.button = null;
+    this.state = {
+      confetti: [],
     };
+    this.confettiArray = new Array(80).fill().map((_, index) => {
+        return <ConfettiParticle key={index}/>
+    });
+  }
+  
+  componentDidMount() {
+    gsap.registerPlugin(Physics2DPlugin);
+  }
 
-    particles(parent, quantity, x, y, minAngle, maxAngle) {
-        let colors = [
-            '#FFFF04',
-            '#EA4C89',
-            '#892AB8',
-            '#4AF2FD',
-        ];
-        for (let i = quantity - 1; i >= 0; i--) {
-            let angle = gsap.utils.random(minAngle, maxAngle),
-                velocity = gsap.utils.random(70, 140),
-                dot = document.createElement('div');
-            dot.style.setProperty('--b', colors[Math.floor(gsap.utils.random(0, 4))]);
-            // console.log('parent', parent, 'dot', dot);
-            // this.confetti.push(dot);
-            this.setState(prevState => ({
-                confetti: [...prevState.confetti, dot]
-            }))
-            console.log(dot)
-            gsap.set(dot, {
-                opacity: 0,
-                x: x,
-                y: y,
-                scale: gsap.utils.random(1.6, 1.7)
-            });
-            gsap.timeline({
-                onComplete() {
-                    dot.remove();
-                }
-            }).to(dot, {
-                duration: .05,
-                opacity: 1
-            }, 0).to(dot, {
-                duration: 1.8,
-                rotationX: `-=${gsap.utils.random(720, 1440)}`,
-                rotationZ: `+=${gsap.utils.random(720, 1440)}`,
-                physics2D: {
-                    angle: angle,
-                    velocity: velocity,
-                    gravity: 120
-                }
-            }, 0).to(dot, {
-                duration: 1,
-                opacity: 0
-            }, .8);
+  popConfetti(elem) {
+    elem.classList.add("success");
+    let ref = this;
+    gsap.to(elem, {
+      "--icon-x": -3,
+      "--icon-y": 3,
+      "--z-before": 0,
+      duration: 0.2,
+      onComplete() {
+        ref.particles(ref.confettiArray, 100, -14, 16);
+        gsap.to(elem, {
+          "--icon-x": 0,
+          "--icon-y": 0,
+          "--z-before": -6,
+          duration: 1,
+          ease: "elastic.out(1, .5)",
+          onComplete() {
+            elem.classList.remove("success");
+          },
+        });
+      },
+    });
+  }
+
+  createConfetti = (particlesNumber) => {
+    const array = new Array(particlesNumber)
+      .fill()
+      .map((_, index) => <ConfettiParticle key={index}/>);
+    this.confettiArray = array;
+  };
+
+  
+
+  particles(particleArray, x, y) {
+    let colors = ["#FFFF04", "#EA4C89", "#892AB8", "#4AF2FD"];
+    console.log('confetti array 2', particleArray);
+
+    const selectedParticles = document.querySelectorAll('.confetti-particle')
+;
+    selectedParticles.forEach(dot => {
+        let angle = gsap.utils.random(0, 180);
+        let velocity = gsap.utils.random(200, 300);
+        console.log(dot._self)
+
+    gsap.timeline().to(
+        dot, {
+            opacity: 1, 
+            duration: 2.8,
+                    rotationX: `-=${gsap.utils.random(720, 1440)}`,
+                    rotationZ: `+=${gsap.utils.random(720, 1440)}`,
+                    physics2D: {
+                      angle: angle,
+                      velocity: velocity,
+                      gravity: 120,
+                      acceleration: 50,
+                      accelerationAngle: 180
+                    },
         }
-    }
+    ).to(dot, {
+        opacity: 0,
+        duration: 1
+    }, '-=1')
+    })
+  }
 
-
-
-    render() {
-        return (
-            <div className="game-content gameover"
-            // ref={div => this.gameover = div}
-            // onClick={() => { this.popConfetti(this.gameover) }}
-            >
-                <div className="loading-container">
-                    <h1><span role="img" aria-label="confetti-popper">🎉</span> XX Team Wins!! <span role="img" aria-label="confetti-popper">🎉</span></h1>
-                </div>
-                <button className="button grey"
-                    ref={div => this.button = div}
-                    onClick={() => { this.popConfetti(this.button) }}
-                >
-                    <div className="icon">
-                        <div className="cannon"></div>
-                        <div className="confetti">
-                            <svg viewBox="0 0 18 16">
-                                <polyline points="1 10 4 7 4 5 6 1" />
-                                <path d="M4,13 C5.33333333,9 7,7 9,7 C11,7 12.3340042,6 13.0020125,4" />
-                                <path d="M6,15 C7.83362334,13.6666667 9.83362334,12.6666667 12,12 C14.1663767,11.3333333 15.8330433,9.66666667 17,7" />
-                            </svg>
-                            <i></i><i></i><i></i><i></i><i></i><i></i>
-                            <div className="emitter"></div>
-                        </div>
-                    </div>
-                    <span>Confirm</span>
-                    {/* {this.state.confetti} */}
-                </button>
-
+  render() {
+    return (
+      <div className="game-content gameover">
+        <div className="loading-container">
+          <h1>
+            <span role="img" aria-label="confetti-popper">
+              🎉
+            </span>{" "}
+            XX Team Wins!!{" "}
+            <span role="img" aria-label="confetti-popper">
+              🎉
+            </span>
+          </h1>
+        </div>
+        {this.confettiArray}
+        <button
+          className="button grey confetti-button"
+          ref={(div) => (this.button = div)}
+          onClick={() => {
+            this.popConfetti(this.button);
+          }}
+        >
+          <div className="icon">
+            <div className="cannon"></div>
+            <div className="confetti">
+              <svg viewBox="0 0 18 16">
+                <polyline points="1 10 4 7 4 5 6 1" />
+                <path d="M4,13 C5.33333333,9 7,7 9,7 C11,7 12.3340042,6 13.0020125,4" />
+                <path d="M6,15 C7.83362334,13.6666667 9.83362334,12.6666667 12,12 C14.1663767,11.3333333 15.8330433,9.66666667 17,7" />
+              </svg>
+              <i></i>
+              <i></i>
+              <i></i>
+              <i></i>
+              <i></i>
+              <i></i>
+              <div className="emitter"></div>
             </div>
-        )
-    }
+          </div>
+          <span>Confirm</span>
+          {/* {this.state.confetti} */}
+        </button>
+      </div>
+    );
+  }
 }
 
+const mapStateToProps = (state) => {
+  return {
+    state: { ...state },
+  };
+};
 
-const mapStateToProps = state => {
-    return {
-        state: { ...state }
-    }
-}
-
-const mapDispatchToProps = dispatch => {
-    return {
-        updateGameData: (gameData) => {
-            const copyGameData = JSON.parse(JSON.stringify(gameData));
-            dispatch(updateGameData(copyGameData));
-        }
-    }
-}
+const mapDispatchToProps = (dispatch) => {
+  return {
+    updateGameData: (gameData) => {
+      const copyGameData = JSON.parse(JSON.stringify(gameData));
+      dispatch(updateGameData(copyGameData));
+    },
+  };
+};
 
 export default connect(mapStateToProps, mapDispatchToProps)(GameOver);
